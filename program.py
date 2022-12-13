dd495a75b7ad094798b92d06067a0ab2f23d785c
PancernyKebab <tytus200200@gmail.com>
2022-12-13 15:34:50 +0100

    protokol dodalem prawie

--- program.py
+++ program.py
@@ -4,6 +4,7 @@ licznik1=0
 licznik2=0
 licznik3=0
 licznik4=0
+licznik5=0
 #zrob wybor normalnego pliku a zamiane w pdf
 #moze jakies gui
 #-------------------------------------------------------------------------------------------------
@@ -215,6 +216,54 @@ while 1:
     #        ws["l21"]=wartoscredu
             wb.save(filename=nazwapliku)
             print("Jeżeli chcesz zrobić kolejny resurs wcisnij enter")
+            koniec=input("Jeżeli chcesz zmienic typ resursu wpisz 'zmien'")
+            if koniec=="zmien":
+                break
+        if wybor=="protokol":
+            wb=load_workbook(filename="Protokolwzor.xlsx")
+            wb.iso_dates=True
+            ws=wb.active
+            licznik4+=1
+            nazwapliku=f'Protokol{licznik5}.xlsx'
+            numer=input("Nnumer protokołu: ")
+            nazwafirmy=input("Nazwa firmy: ")
+            typ=input("Typ: ")
+            nrser=input("Numer seryjny: ").lower()
+            stanl=int(input("Stan licznika: "))
+            udzwig=input("Udźwig: ")
+            cykle=input("Ilość cykli: ")
+            dnipracy=input("Ilość dni pracy (domyślnie 240): ")
+            if dnipracy=="":
+                dnipracy=240
+            wartoscgran=input("Wartość graniczna (domyślnie 60000): ")
+            if wartoscgran=="":
+                wartoscgran=60000
+    #        wspolczynnik=str(input("Współczynnik (domyślny 1.0): "))
+    #        wspolczynnik=wspolczynnik.replace(",",".")
+    #        wspolczynnik=float(wspolczynnik)
+    #        if wspolczynnik=="":
+    #            wspolczynnik=1.0
+    #        wartoscredu=str(input("Wartość redukująca 100 lub 125 procent: "))
+    #        wartoscredu.replace("%","")
+    #        wartoscredu=float(wartoscredu)
+    #        if wartoscredu==100:
+    #            wartoscredu=1
+    #        else:
+    #            wartoscredu=1.25
+            razem=typ.upper()+" "+produ.capitalize()
+            ws["d7"]=ekspl.capitalize()
+            ws["d9"]=nrewi.upper()
+            ws["n8"]=razem
+            ws["n9"]=nrser.upper()
+            ws["n10"]=rok
+            ws["n11"]=udzwig
+            ws["n12"]=cykle
+            ws["c22"]=dnipracy
+    #        ws["i21"]=wspolczynnik
+            ws["g22"]=wartoscgran
+    #        ws["l21"]=wartoscredu
+            wb.save(filename=nazwapliku)
+            print("Jeżeli chcesz zrobić kolejny protokół wcisnij enter")
             koniec=input("Jeżeli chcesz zmienic typ resursu wpisz 'zmien'")
             if koniec=="zmien":
                 break
